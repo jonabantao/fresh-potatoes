427ceb7ce2f819b75b94e171f180404f8cc49e5a
Jonathan Abantao <jonabantao@gmail.com>
2018-04-21 19:41:28 -0700

    Set up linter and initalize sequelize

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-const sqlite = require('sqlite'),
-      Sequelize = require('sequelize'),
-      request = require('request'),
-      express = require('express'),
-      app = express();
+const sqlite = require('sqlite');
+const Sequelize = require('sequelize');
+const request = require('request');
+const express = require('express');
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+const app = express();
+
+const { PORT = 3000, NODE_ENV = 'development', DB_PATH = './db/database.db' } = process.env;
 
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
-// ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
+// INITIALIZE SEQUALIZE
+const sequelize = new Sequelize('main', 'user', 'supersecurepassword', {
+  dialect: 'sqlite',
+  storage: DB_PATH,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  console.log(req.params.id);
+  res.status(200).send({
+    recommendations: [],
+    meta: {},
+  }).json();
 }
+// ROUTES
+app.get('/films/:id/recommendations', getFilmRecommendations);
+
 
 module.exports = app;
